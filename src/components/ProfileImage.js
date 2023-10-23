9cf36e933fc407b3c213c9b7ba9df197813b5274
Alicia <arrowsrp@yahoo.com>
2023-10-22 22:28:11 -0700

    Centers profile-image.

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
 export default function ProfileImage () {
-    const [activeAttributeIndex, setActiveAttributeIndex] = useState(0);
-    const attributes = ["Community", "Software", "Tiny-house"];
-
+    
     return(
-        <div>
-            <p>Alicia Parris: <span>{attributes[activeAttributeIndex]}</span> <span>builder.</span></p>
-        <div className="overlay-container" id="profile-image-div"></div>
+        <div id='profile-image-container'>
+            <p>Alicia Parris:</p> <div className="attributes-text"><span>community</span><span>tiny-home</span><span>software</span></div> <p>builder.</p>
+            <div className="overlay-container" id="profile-image-div"></div>
         </div>
     )
 }
