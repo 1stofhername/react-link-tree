192aa3ee2356e86bc6e36f492414cefd7585408a
Alicia <arrowsrp@yahoo.com>
2023-10-29 16:06:22 -0700

    Restructures attributes to cycle using css animation.

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 
 export default function ProfileImage () {
-    
+
     return(
         <div id='profile-image-container'>
-            <p className="text">Alicia Parris:</p> <div className="attributes-text"><span>community</span><span>tiny-home</span><span>software</span></div> <p>builder.</p>
+            <p className="text">Alicia Parris:</p> 
+            <div className="attributes-container">
+            <div className="attributes-text" >
+                <p>community</p><p>tiny-home</p><p>software</p>
+            </div> 
+            </div>
+                <p>builder.</p>
             <div className="overlay-container" id="profile-image-div"></div>
         </div>
     )
