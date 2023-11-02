2d343f00c2d44a5abeeac33dc2f019004b8e3c05
Alicia <arrowsrp@yahoo.com>
2023-11-02 15:54:36 -0700

    Adds css text attribute animation.

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import ProfileImage from './ProfileImage';
 import Header from './Header';
 import '../App.css';
 
-
+//Add get in touch button - email
+//Add subscribe button to capture emails.
+//Setup database
 function App() {
   return (
     <div className="App">
