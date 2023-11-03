a672514abcb60a55c671427702bcb9e4186e4cc9
Alicia <arrowsrp@yahoo.com>
2023-11-02 17:27:08 -0700

    Adds div wrapper around ProfileImage text.

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -4,13 +4,15 @@ export default function ProfileImage () {
 
     return(
         <div id='profile-image-container'>
-            <p className="text">Alicia Parris:</p> 
-            <div className="attributes-container">
-            <div className="attributes-text" >
-                <p>community</p><p>tiny-home</p><p>software</p>
-            </div> 
-            </div>
+            <div className="profile-text-container">
+                <p className="text">Alicia Parris</p> 
+                <div className="attributes-container">
+                <div className="attributes-text" >
+                    <p>community</p><p>tiny-home</p><p>software</p>
+                </div> 
+                </div>
                 <p>builder.</p>
+            </div>
             <div className="overlay-container" id="profile-image-div"></div>
         </div>
     )
